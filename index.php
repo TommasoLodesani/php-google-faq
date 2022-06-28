d1ca8d279190199dc56ab652e65c34160665cc7c
Tommaso <tommaso.lodesani@gmail.com>
2022-06-28 15:21:05 +0200

    setting project

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$articles = [
+    [
+        'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        'answer'   => ""
+
+    ],
+    [
+        'question' => "",
+        'answer'   => ""
+
+    ],
+    [
+        'question'  => "",
+        'sub_title' => "",
+        'answer'     => ""
+
+    ],
+    [
+        'question' => "",
+        'answer'   => ""
+
+    ],
+    [
+        'question' => "",
+        'answer'   => ""
+
+    ],
+
+]
+
+?>
